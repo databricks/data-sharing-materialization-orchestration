400c60032eaef8c9fc4acdef3198d4c51300d6b1
Patrick Jin <patrick.jin@databricks.com>
2024-09-24 17:56:50 -0700

    support refreshing multiple schemas (for batching)

--- refresh_task.py
+++ refresh_task.py
@@ -6,7 +6,7 @@ def main():
       --table
           The foreign table to be refreshed.
       --schema
-          The foreign schema to be refreshed.
+          A foreign schema to be refreshed. May occur multiple times to denote multiple schemas being refreshed.
     """
     args = parse_args()
 
@@ -14,14 +14,15 @@ def main():
         table_full_name = sanitize(args["table"])
         spark.sql(f"REFRESH FOREIGN TABLE {table_full_name}")
     elif "schema" in args:
-        schema_full_name = sanitize(args["schema"])
-        """
-        We use SHOW TABLES instead of REFRESH FOREIGN SCHEMA because REFRESH 
-        FOREIGN SCHEMA refreshes table-level metadata of every table in addition 
-        to schema metadata, while SHOW TABLES only refreshes schema metadata.
-        As such, SHOW TABLES is faster.
-        """
-        spark.sql(f"SHOW TABLES IN {schema_full_name}")
+        for schema_full_name in args["schema"]:
+            schema_full_name_sanitized = sanitize(schema_full_name)
+            """
+            We use SHOW TABLES instead of REFRESH FOREIGN SCHEMA because REFRESH 
+            FOREIGN SCHEMA refreshes table-level metadata of every table in addition 
+            to schema metadata, while SHOW TABLES only refreshes schema metadata.
+            As such, SHOW TABLES is faster.
+            """
+            spark.sql(f"SHOW TABLES IN {schema_full_name_sanitized}")
 
 if __name__ == '__main__':
     main()
